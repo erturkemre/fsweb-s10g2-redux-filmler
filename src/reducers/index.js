b26b405bca418e6d5862e0cf16d3fc80ba8f011a
Emre <emre.ertrk.3455@gmail.com>
2023-11-09 15:45:18 +0300

    tasks completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import favoriteReducer from "./favoriteReducer";
+import movieReducer from "./movieReducer";
 
-import movieReducer from './movieReducer';
+const rootReducer = combineReducers({
+  movieReducer,
+  favoriteReducer,
+});
 
-export default movieReducer;
+export default rootReducer;
